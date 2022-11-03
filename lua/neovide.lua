b330b4d3528ed057c9d85a1e04c4fa9b628b19f5
yousiki <you.siki@outlook.com>
2022-11-03 10:55:24 +0800

    just add neovide configs though it's not working

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.setup()
   vim.opt.guifont = "Delugia:h12"
   -- TODO: The following lines have no efforts on macOS, to be fixed.
-  -- vim.g.neovide_remember_window_size = true
-  -- vim.g.neovide_remember_window_position = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_remember_window_position = true
 end
 
 return M
